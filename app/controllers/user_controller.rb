64d1cbe55a06cfaa8f037eb077b9d3d5bde8ae3f
Yash Yadav <yyadav@academic.rrc.ca>
2024-04-12 06:43:11 -0500

    updated user_controller

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -12,7 +12,7 @@ class UserController < ApplicationController
     @customer = User.find(params[:id])
 
     if @customer.update(customer_params)
-      # Update successful, handle accordingly (e.g., redirect to another page)
+      # Update successful, handle accordingly (e.g., redirect to another e)
       redirect_to '/account', notice: 'Customer information updated successfully.'
     else
       # Update failed, render the edit page again with error messages
